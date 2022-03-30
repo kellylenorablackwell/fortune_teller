585711c62fc83016e29d81776aebcc968e4ea164
Kelly Blackwell <kellylenorablackwell@gmail.com>
2022-03-30 11:08:04 -0400

    coded functionality that displays fortune in correct spot

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
+const $fortune = $('#fortune')
+const $submit = $('#submit')
 
 const listOfAnswers = ['Yes', 'Of Course','Certainly', 'Definitely', 'Undoubtedly', 'Unquestionably','Affirmative', 'Without a shadow of a doubt','No', 'Not Likely','Never', 'No way', 'I would not hold my breath', 'Not at all', 'Absolutely not', 'Maybe', 'Possibly', 'It could happen', 'Perhaps', 'Probably', 'It is likely']
 
-const actualAnswer = ()=>{
-    console.log(listOfAnswers[Math.floor((Math.random() * 21) + 1)])
-}
+const actualAnswer = listOfAnswers[Math.floor((Math.random() * 20) + 1)]
 
-actualAnswer()
+
 
 $( () => {
-    //jQuery code goes here
+    
+    $submit.on('click', ()=>{
+        $fortune.text(actualAnswer).fadeOut(5000)
+        event.preventDefault()
+    
+    })
+
+
+
+
+
+
+
+
+
   });
