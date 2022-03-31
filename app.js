3608a8a4dafd29b04c2c5bf526154cc5e6410c81
Kelly Blackwell <kellylenorablackwell@gmail.com>
2022-03-31 00:10:31 -0400

    added header, footer, centered image, centered description and centered question sections

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const $fortune = $('#fortune')
 const $submit = $('#submit')
 
-const listOfAnswers = ['Yes', 'Of Course','Certainly', 'Definitely', 'Undoubtedly', 'Unquestionably','Affirmative', 'Without a shadow of a doubt','No', 'Not Likely','Never', 'No way', 'I would not hold my breath', 'Not at all', 'Absolutely not', 'Maybe', 'Possibly', 'It could happen', 'Perhaps', 'Probably', 'It is likely']
+const listOfAnswers = ['Yes', 'Of Course','Certainly', 'Definitely', 'I doubt it', 'Unquestionably','Affirmative', 'Without a doubt','No', 'Not Likely','Never', 'No way', 'Do not hold your breath', 'Unlikely', 'Not at all', 'Absolutely not', 'Maybe', 'Possibly', 'It could happen', 'Perhaps', 'Probably', 'It is likely']
 
 const actualAnswer = listOfAnswers[Math.floor((Math.random() * 20) + 1)]
 
@@ -12,7 +12,9 @@ $( () => {
     $submit.on('click', ()=>{
         $fortune.text(actualAnswer).fadeOut(5000)
         event.preventDefault()
-    
+        setTimeout(()=>{
+            location.reload()
+        }, 5000)
     })
 
 
